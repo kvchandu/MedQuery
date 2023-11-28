48b4cafeda969d6910deebfbeed76b11d71c0fb7
kvchandu <kchandu@ucsd.edu>
2023-11-27 16:33:52 -0800

    Conversation Screen Added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -56,11 +57,38 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    val nav_version = "2.7.5"
+
+    // Java language implementation
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+
+    // Jetpack Compose Integration
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
 }
--- app/src/main/java/com/example/medquery/MainActivity.kt
+++ app/src/main/java/com/example/medquery/MainActivity.kt
@@ -4,19 +4,25 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,35 +31,47 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.compose.material3.TextField
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.foundation.layout.heightIn
+
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+
+
 import com.example.medquery.ui.theme.MedQueryTheme
 
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var startButton: Button
-    private lateinit var recognizedText: TextView
-    private lateinit var speechRecognizer: SpeechRecognizer
 
+    private lateinit var speechRecognizer: SpeechRecognizer
+    private val chatBoxText = mutableStateOf("")
+    private val conversationMessages = mutableStateListOf<GPTMessage>()
+    data class GPTMessage(val role: String, val message: String)
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        startButton = findViewById(R.id.startButton)
-        recognizedText = findViewById(R.id.resultTextView)
-
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.RECORD_AUDIO
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.RECORD_AUDIO),
-                1
-            )
-        }
-
-        // Initialize SpeechRecognizer
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
         speechRecognizer.setRecognitionListener(object : RecognitionListener {
             override fun onReadyForSpeech(params: Bundle?) {}
@@ -65,7 +83,9 @@ class MainActivity : ComponentActivity() {
             override fun onResults(results: Bundle?) {
                 val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
                 if (matches != null && matches.isNotEmpty()) {
-                    recognizedText.text = matches[0]
+                    chatBoxText.value = matches[0]
+                    conversationMessages.add(GPTMessage("client", matches[0]))
+                    Log.d("TEST", conversationMessages.size.toString())
                 }
             }
 
@@ -73,14 +93,35 @@ class MainActivity : ComponentActivity() {
             override fun onEvent(eventType: Int, params: Bundle?) {}
         })
 
-        // Set onClickListener for the startButton
-        startButton.setOnClickListener {
-            startSpeechToText()
+        setContent {
+            MedQueryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    Scaffold(topBar = {}, bottomBar = {BottomBar()}){
+                            innerPadding ->
+                        LazyColumn (modifier = Modifier
+                                .padding(innerPadding)){
+                            items(conversationMessages.size) {
+                                index -> MessageBox(message = conversationMessages[index])
+                            }
+                        }
+
+                    }
+                }
+            }
         }
-
-
     }
 
+
+
+    @Composable
+    private fun ConversationArea(modifier: Modifier = Modifier){
+        Text(text = "Hi")
+    }
     private fun startSpeechToText() {
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         intent.putExtra(
@@ -101,24 +142,38 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
         speechRecognizer.destroy()
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface (color = Color.Gray) {
-        Text(
-            text = "How are you $name!",
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            modifier = modifier.padding(24.dp)
+
+    @Composable
+    fun MessageBox(message: GPTMessage, modifier: Modifier = Modifier){
+        Text("${message.role} : ${message.message}" )
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ChatBox(
+        modifier: Modifier = Modifier
+    ) {
+        TextField(
+            value = chatBoxText.value,
+            onValueChange = {},
+            modifier = modifier
+                .heightIn(min = 56.dp)
         )
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MedQueryTheme {
-        Greeting("Android")
+    @Composable
+    fun ChatButton(modifier: Modifier = Modifier) {
+        Button(onClick = { startSpeechToText()}){
+            Text("Filled")
+        }
+    }
+
+    @Composable
+    fun BottomBar(modifier: Modifier = Modifier){
+        Row () {
+            ChatBox(modifier = Modifier.weight(0.75f))
+            ChatButton(modifier = Modifier.weight(0.25f))
+        }
     }
 }
